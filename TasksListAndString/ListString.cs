1034e8b7cc46e29afa6c6346c073fb759d91e1e0
prajdziswet <prajdziswet@mail.ru>
2021-03-09 13:29:23 +0300

    first

---
+++ TasksListAndString/ListString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace TasksListAndString
+{
+    public static class ListString
+    {
+        //return Max in list
+        public static int Task_1(List<int> temp)
+        {
+            if (temp.Count == 0) throw new Exception("the list is zero-length");
+            return temp.Max();
+        }
+    }
+}
---
+++ TasksListAndString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TasksListAndString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> _list = new List<int>(){1,2,5,8,99,45,55,6};
+
+            try
+            {
+                Console.WriteLine("Наибольшее число списка: "+ListString.Task_1(_list));
+            }
+            catch(Exception e)
+            { Console.WriteLine(e.Message);}
+
+            //заглушка
+            Console.ReadKey();
+        }
+    }
+}
