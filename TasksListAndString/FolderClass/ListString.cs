65c8a10a4cf3dd2cb3d84d44a5a4680b5d66c254
Prajdziswet <prajdziswet@mail.ru>
2021-03-24 16:26:09 +0300

    brr

--- TasksListAndString/FolderClass/ListString.cs
+++ TasksListAndString/FolderClass/ListString.cs
@@ -153,10 +153,30 @@ namespace TasksListAndString
         #endregion
 
         #region Exercise 14-
+        //17 Implement the following sorting algorithms: Selection sort, Insertion sort, Merge sort, Quick sort, Stooge Sort.Check Wikipedia for descriptions.
+        // Selection sort
+        public static void Exercise_17_1(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[min])
+                    {
+                        min = j;
+                    }
+                }
+                int dummy = list[i];
+                list[i] = list[min];
+                list[min] = dummy;
+            }
 
+            ShowMessageList(list, "Сортированный-выборкой массив: ");
+        }
 
         #endregion
 
 
-}
+    }
 }
