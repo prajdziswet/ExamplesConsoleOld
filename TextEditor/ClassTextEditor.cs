9f3c686b99e9159f938050ed1731ae52fd51709d
Prajdziswet <prajdziswet@mail.ru>
2021-04-01 10:25:23 +0300

    TextEditor on 1 lines Console

---
+++ TextEditor/ClassTextEditor.cs
@@ -0,0 +1,121 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Remoting.Messaging;
+    using System.Threading;
+    using System.Windows;
+ 
+
+    namespace TextEditor
+    {
+        public class Text
+        {
+            public string str;
+            public int position;
+
+            public Text(string str, int position)
+            {
+                this.str = str;
+                this.position = position;
+            }
+        }
+
+        public class ClassTextEditor
+    {
+        #region value and internal function
+        public ClassTextEditor()
+            {
+            }
+
+            private List<Text> _fields=new List<Text>(){ new Text("", 0) };
+            private int _currentField = 0;
+            private int CurrentField
+            {
+                get => _currentField;
+                set
+                {
+                    if (_fields == null || _fields.Count != 0 && value > 0 && value < _fields.Count)
+                        _currentField = value;
+                }
+
+            }
+            private int _currentPosition = 0;
+            public int CurrentPosition
+            {
+                get => _currentPosition;
+                set
+                {
+                    if (_fields == null || _fields.Count != 0 && value >= 0 && value <= Fields.str.Length)
+                        _currentPosition = value;
+                }
+
+            }
+
+            public Text Fields
+            {
+                get=>_fields[_currentField];
+
+                set
+                {
+                    if (value != null)
+                    {
+                        _fields.Add(value);
+                        CurrentField++;
+                }
+                }
+            }
+
+
+            //INSERT {text} - appends {text} to output;
+            public void Insert(string text)
+            {
+                Text t1 = new Text("", 0);
+                t1.str = Fields.str.Insert(_currentPosition, text);
+                t1.position = _currentPosition + text.Length;
+                Fields = t1;
+                CurrentPosition = t1.position;
+            }
+            #endregion
+
+        //DELETE - deletes the last symbol from output(does nothing if output is empty);
+        public void Delete()
+        {
+            if (!String.IsNullOrEmpty(Fields.str))
+            {
+                Text text=new Text("",0);
+                if (_currentPosition >= 1) text.str = Fields.str.Remove(_currentPosition - 1,1);
+                text.position = _currentPosition - 1;
+                Fields = text;
+                CurrentPosition--;
+            }
+        }
+
+        //COPY {index} - copies a substring of output starting from {index} and to the end (does nothing if {index} is out of range)
+        //I didn't copy to end
+        public void Copy(int startPosition, int finishPosition)
+        {
+            String newText = Fields.str.Substring(startPosition, finishPosition - startPosition);
+            Clipboard.Clear();
+            Clipboard.SetText(newText);
+        }
+
+        
+        //PASTE - appends copied text to output (does nothing if nothing has been copied);
+        public void Paste()
+        {
+            if (Clipboard.ContainsText())
+            {
+                String newText = Clipboard.GetText();
+                Insert(newText);
+            }
+        }
+        //UNDO - undoes one last successful operation (INSERT/DELETE/PASTE). can be called multiple times in a row to undo multiple operations.
+        public void Undo()
+        {
+            if (CurrentField>0)
+            {
+                CurrentField = CurrentField - 1;
+            }
+        }
+    }
+
+}
---
+++ TextEditor/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace TextEditor
+{
+    class Program
+    {
+            static ClassTextEditor classTextEditor = new ClassTextEditor();
+
+            //bool spectrum
+            static void boolSpectrum(ref int X,int temp)
+            {
+                if (temp < 0) X = 0;
+                else if (temp > classTextEditor.Fields.str.Length) X = classTextEditor.Fields.str.Length - 1;
+                else if (temp >= 0 && temp < classTextEditor.Fields.str.Length) X = temp;
+            }
+
+            static void ChaingeSelect(ref bool select, ref int startIndex, ref int finishIndex, bool plus)
+            {
+            if (select == false)
+            {
+                select = true;
+                boolSpectrum(ref startIndex, classTextEditor.CurrentPosition);
+                if (plus) boolSpectrum(ref finishIndex, classTextEditor.CurrentPosition + 1);
+                else boolSpectrum(ref finishIndex, classTextEditor.CurrentPosition-1);
+            }
+            else
+            {
+                if (plus) boolSpectrum(ref finishIndex, finishIndex+1);
+                else boolSpectrum(ref finishIndex, finishIndex - 1);
+            }
+
+            if (startIndex == finishIndex) select = false;
+
+            }
+        
+            [STAThreadAttribute]
+            static void Main(string[] args)
+        {
+            //select
+            bool sel = false;
+            int startIndexSelect = -1, finishIndexSelect = -1;
+
+
+
+            Console.TreatControlCAsInput = true;
+            Console.WriteLine("esc = exit, ctr+z = undo, ctr+left(or Right) = select, ctrl+C = Copy, ctr+V = Insert, del (or Backspace) = del");
+            ConsoleKeyInfo key;
+
+            while (true)
+            {
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                //вставка ctrl+c
+                if (key.Modifiers == ConsoleModifiers.Control && key.Key == ConsoleKey.C&&sel)
+                {
+                    if (startIndexSelect > finishIndexSelect) classTextEditor.Copy(finishIndexSelect, startIndexSelect);
+                    else classTextEditor.Copy( startIndexSelect, finishIndexSelect);
+                    sel = false;
+                    UpdateText();
+                    continue;
+                }
+
+                //вставка ctrl+V
+                if (key.Modifiers ==ConsoleModifiers.Control&& key.Key == ConsoleKey.V)
+                {
+                    classTextEditor.Paste();
+                    UpdateText();
+                    continue;
+                }
+
+                //press ctrl+z
+                if (key.Modifiers == ConsoleModifiers.Control && key.Key == ConsoleKey.Z)
+                {
+                    classTextEditor.Undo();
+                    UpdateText();
+                    continue;
+                }
+
+                //press shift+left(+rigth)
+                if (key.Modifiers == ConsoleModifiers.Shift && key.Key == ConsoleKey.LeftArrow)
+                    ChaingeSelect(ref sel, ref startIndexSelect, ref finishIndexSelect, false);
+                else if (key.Modifiers == ConsoleModifiers.Shift && key.Key == ConsoleKey.RightArrow)
+                    ChaingeSelect(ref sel, ref startIndexSelect, ref finishIndexSelect, true);
+
+                //cancel selected
+                if (key.Key == ConsoleKey.Insert) sel = false;
+
+                if (sel)
+                {
+                    SelectText(startIndexSelect,finishIndexSelect);
+                    continue;
+                }
+
+                //press button left or Rigth
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    classTextEditor.CurrentPosition--;
+                    Console.SetCursorPosition(classTextEditor.CurrentPosition, 1);
+                    continue;
+                }
+                else if (key.Key == ConsoleKey.RightArrow)
+                {
+                    classTextEditor.CurrentPosition++;
+                    Console.SetCursorPosition(classTextEditor.CurrentPosition, 1);
+                    continue;
+                }
+
+                //press backspace or del
+                if (key.Key == ConsoleKey.Backspace | key.Key == ConsoleKey.Delete)
+                {
+                    classTextEditor.Delete();
+                    UpdateText();
+                    continue;
+                }
+
+                if (key.KeyChar == '\u0000') continue;
+                else
+                {
+                   classTextEditor.Insert(key.KeyChar.ToString());
+                   UpdateText();
+                }
+
+
+                
+            }
+        }
+
+            private static void UpdateText()
+            {
+            Console.SetCursorPosition(0, 1);
+            Console.Write(new String(' ', Console.BufferWidth));
+            Console.SetCursorPosition(0, 1);
+            Console.Write(classTextEditor.Fields.str);
+            Console.SetCursorPosition(classTextEditor.CurrentPosition, 1);
+            }
+
+            private static void SelectText(int startIndex,int finishIndex)
+            {
+                if (startIndex > finishIndex)
+                {
+                    int temp = finishIndex;
+                    finishIndex = startIndex;
+                    startIndex = temp;
+                }
+                Console.SetCursorPosition(0, 1);
+                int index = 0;
+                foreach (var ch in classTextEditor.Fields.str)
+                {
+                    if (index >= startIndex && index < finishIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+
+                    index++;
+                    Console.Write(ch);
+                }
+            Console.SetCursorPosition(classTextEditor.CurrentPosition, 1);
+        }
+    }
+}
