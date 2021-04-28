7137b27e4a73582ce2a02ff9c900e5524ff7dd5f
Prajdziswet <prajdziswet@mail.ru>
2021-04-28 11:39:59 +0300

    add two class

---
+++ ClassLibrary/ClassLibrary/CardReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class CardReader
+    {
+        public int NumberCard;
+        public Reader Reader;
+        public IEnumerable<int> ISBNs;
+    }
+}
