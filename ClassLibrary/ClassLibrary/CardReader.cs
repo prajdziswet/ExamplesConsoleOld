7a72cf3ad7282c4a1d5d3ba1b29d41c70ae97b1a
Prajdziswet <prajdziswet@mail.ru>
2021-05-12 07:46:10 +0300

    please, look at comment

--- ClassLibrary/ClassLibrary/CardReader.cs
+++ ClassLibrary/ClassLibrary/CardReader.cs
@@ -5,8 +5,49 @@ namespace ClassLibrary
 {
     public class CardReader
     {
-        public int ID;
-        public Reader Reader;
-        public IEnumerable<Book> Books;
+        public int ID
+        {
+            get;
+            private set;
+        }
+        public Reader Reader
+        {
+            get;
+            private set;
+        }
+        public List<Book> Books
+        {
+            get;
+            private set;
+        }
+        public static int Count
+        {
+            get;
+            private set;
+        }
+
+        public CardReader(Reader reader)
+        {
+            //could better check here or all the same Library??
+            if (reader == null)
+            {
+                throw new NullReferenceException("Readers reference is Null");
+            }
+
+            ID = ++Count;
+            Reader = reader;
+        }
+
+        public void AddBookInCardReader(Book book)
+        {
+            if (book == null)
+            {
+                throw new NullReferenceException("Book's reference is Null (received in AddBookInCardReader)");
+            }
+            else if (book.AuthorBook == null || book.ISBN.IsNullOrWhiteSpace() || book.NameBook.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("AddBookInCardReader received incorrect arguments");
+            }
+        }
     }
 }
--- ClassLibrary/ClassLibrary/Reader.cs
+++ ClassLibrary/ClassLibrary/Reader.cs
@@ -4,8 +4,43 @@ namespace ClassLibrary
 {
     public class Reader
     {
-        public int ID;
-        public String Name;
-        public String LastName;
+        public int ID
+        {
+            get;
+            private set;
+        }
+        public String Name
+        {
+            get;
+            private set;
+        }
+        public String LastName
+        {
+            get;
+            private set;
+        }
+
+        public static int Count
+        {
+            get;
+            private set;
+        }
+
+        public Reader(String Name, String LastName)
+        {
+            //maybe better check here or all the same Library??
+            if (Name.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("Name is empty");
+            }
+            else if (LastName.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("Name is empty");
+            }
+
+            ID=++Count;
+            this.Name = Name;
+            this.LastName = LastName;
+        }
     }
 }
