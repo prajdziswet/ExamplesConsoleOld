cd60af430ff594010c4924cb522f8f3016b0f96d
Prajdziswet <prajdziswet@mail.ru>
2021-06-07 08:00:50 +0300

    correct inherit

--- ClassLibrary/ClassLibrary/BorrowedBook.cs
+++ ClassLibrary/ClassLibrary/BorrowedBook.cs
@@ -9,14 +9,24 @@ namespace ClassLibrary
             get;
             private set;
         }
-        private DateTime _dateTime=DateTime.Now;
+
         public DateTime dateTime
         {
-            get => _dateTime;
-        }
+            get;
+            //private set;
+            //so the field is set only once when the class is created? (without set) 
+        } = DateTime.Now;
+
         public BorrowedBook(Book book):base(book.ISBN,book.NameBook,book.AuthorBook)
         {
             ID=book.ID;
+        }
+
+        //why is it better? 
+        public BorrowedBook(int ID,String ISBN, String NameBook, Author AuthorBook) 
+            : base(ISBN, NameBook, AuthorBook)
+        {
+            this.ID = ID;
         }
     }
 }
